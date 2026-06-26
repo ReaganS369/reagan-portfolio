785e09b9951393e4d90aad25f2eaadc09be8b92b
ReaganS369 <reagans369@gmail.com>
2026-06-27 01:32:50 +0530

    feat(admin): add hero roles management CMS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ import HomeHero from '../components/hero/HomeHero';
 import Contact from '../components/sections/Contact';
 import Hero from '../components/sections/Hero';
 import Projects from '../components/sections/Projects';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <HomeHero />
-    </>
-  );
+  return redirect('/admin');
 }
